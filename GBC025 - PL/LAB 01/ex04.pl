a8f0c7d504b539a965847971ceac3bfde50d616d
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2023-10-02 16:17:34 -0300

    Update ex04.pl

--- GBC025 - PL/LAB 01/ex04.pl
+++ GBC025 - PL/LAB 01/ex04.pl
@@ -18,3 +18,18 @@ livro('Orgulho e Preconceito', 1813, 'J. Austen').
 livro('A Rosa do Povo', 1945, 'C. D. Andrade').
 livro('O Alquimista', 1988, 'P. Coelho').
 livro('Os Miseráveis', 1862, 'V. Hugo').
+
+% Quais os livros de um determinado autor?
+% livro(X, _, 'M. Assis').
+
+% Existe um livro na base publicado em um dado ano?
+% livro(X, 2023, _).
+
+% Quais os livros escritos nos anos 2015 e 2023?
+% livro(X, Y, _), Y >= 2015, Y < 2023.
+
+% Quais os livros escritos antes de um dado ano?
+% livro(X, Y, _), Y < 2020.
+
+% Qual ano um dado título foi publicado?
+% livro('Extraordinário', Y, _).
