94f26450e6fa9d6c1e622f25b213b1c690a20ee8
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2023-11-03 21:49:31 -0300

    Create ex03.pl

---
+++ GBC025 - PL/LAB 05/ex03.pl
@@ -0,0 +1,18 @@
+% d(número, nome).
+d(0, zero).
+d(1, um).
+d(2, dois).
+d(3, tres).
+d(4, quatro).
+d(5, cinco).
+d(6, seis).
+d(7, sete).
+d(8, oito).
+d(9, nove).
+
+% txt(D, P).
+txt([], []).
+
+txt([D|A], [P|B]) :-
+    d(D, P),
+    txt(A, B).
