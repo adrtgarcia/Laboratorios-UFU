f8f751d3ea95372eb925c64389dc51058b085e8b
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2023-11-07 17:34:00 -0300

    Create questao-5.pl

---
+++ GBC025 - PL/TCD/questao-5.pl
@@ -0,0 +1,27 @@
+% LETRA A ----------------------------------------
+
+% estrada(identificador, origem, destino, custo).
+
+estrada(a, cb, ca, 150).
+estrada(b, ca, cb, 90).
+estrada(c, cb, cc, 211).
+estrada(d, cg, ca, 300).
+estrada(e, cb, cd, 50).
+estrada(f, cb, ce, 89).
+estrada(g, ca, cg, 187).
+estrada(h, cd, ch, 254).
+estrada(i, cf, ci, 621).
+estrada(j, cc, cj, 300).
+estrada(k, ci, cj, 41).
+estrada(l, cd, cl, 99).
+estrada(m, ch, cm, 148).
+estrada(n, cp, cm, 163).
+estrada(o, cp, cn, 69).
+estrada(p, cp, cl, 10).
+estrada(q, cj, cl, 364).
+estrada(r, cr, cl, 79).
+estrada(s, cl, co, 193).
+estrada(t, cp, cp, 311).
+estrada(u, cp, cr, 577).
+estrada(v, cn, cp, 150).
+estrada(x, ca, cd, 100).
