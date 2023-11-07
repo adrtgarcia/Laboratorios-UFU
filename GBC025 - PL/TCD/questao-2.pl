4a32c567378ad4308a3a7beaff49535380f22c52
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2023-11-07 17:12:26 -0300

    Create questao-2.pl

---
+++ GBC025 - PL/TCD/questao-2.pl
@@ -0,0 +1,47 @@
+% LETRA A ----------------------------------------
+% L2 é a lista L1 com o elemento X
+
+adiciona(X, L1, L2) :-
+  //
+
+% LETRA B ----------------------------------------
+% L2 é a lista L1 sem o elemento X
+
+apaga(X, L1, L2) :– 
+  //
+
+% LETRA C ----------------------------------------
+% TRUE se X pertencer à lista L
+
+membro(X,L) :-
+  //
+
+% LETRA D ----------------------------------------
+% L3 é a junção das listas L1 e L2
+
+concatena(L1, L2, L3) :– 
+  //
+
+% LETRA E ----------------------------------------
+% X é o número de elementos da lista L
+
+comprimento(X,L) :–
+  //
+
+% LETRA F ----------------------------------------
+% X é o valor máximo da lista L
+
+maximo(X,L) :– 
+  //
+
+% LETRA G ----------------------------------------
+% X é o valor médio da lista L
+
+media(X, L) :– 
+  //
+
+% LETRA H ----------------------------------------
+% diz se a lista L está ordenada (ascendentemente).
+
+ordenada(L) :- 
+  //
