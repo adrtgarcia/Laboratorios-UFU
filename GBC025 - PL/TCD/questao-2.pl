ab43d30e9bdfc858bb5b61144bdacc4e24d47ad6
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2023-11-10 15:59:48 -0300

    Update ex02.pl

--- GBC025 - PL/TCD/questao-2.pl
+++ GBC025 - PL/TCD/questao-2.pl
@@ -3,44 +3,74 @@
 
 adiciona(X, L1, [X|L1]).
 
+
 % LETRA B ----------------------------------------
 % L2 é a lista L1 sem o elemento X
 
-apaga(X, [X|L2], L2) :– 
-  //
+apaga(X, [X|L], L).
+apaga(X, [Y|L1], [Y|L2]) :-
+    apaga(X, L1, L2).
+
 
 % LETRA C ----------------------------------------
 % TRUE se X pertencer à lista L
 
-membro(X,L) :-
-  //
+membro(X, [X|_]).
+membro(X,[_|L]) :-
+    membro(X, L).
+
 
 % LETRA D ----------------------------------------
 % L3 é a junção das listas L1 e L2
 
-concatena(L1, L2, L3) :– 
-  //
+concatena([], L, L).
+concatena([X|L1], L2, [X|L3]) :-
+    concatena(L1, L2, L3).
+
 
 % LETRA E ----------------------------------------
 % X é o número de elementos da lista L
 
-comprimento(X,L) :–
-  //
+comprimento(0, []).
+comprimento(X, [_|L]) :-
+    comprimento(X1, L),
+    X is X1 + 1.
+
 
 % LETRA F ----------------------------------------
 % X é o valor máximo da lista L
 
-maximo(X,L) :– 
-  //
+max(X,[X]).
+max(X,[Y|R]) :- 
+    max(X,R), 
+    X > Y, !.
+max(Y,[Y|_]).
+
 
 % LETRA G ----------------------------------------
 % X é o valor médio da lista L
 
-media(X, L) :– 
-  //
+somaElementos(0, []).
+somaElementos(X, [Y|L]) :-
+    somaElementos(S, L),
+    X is S + Y.
+
+contElementos(0, []).
+contElementos(X, [_|L]) :-
+    contElementos(X1, L),
+    X is X1 + 1.
+
+media(X, L) :-
+    somaElementos(S, L),
+    contElementos(C, L),
+    X is S/C.
+
 
 % LETRA H ----------------------------------------
 % diz se a lista L está ordenada (ascendentemente).
 
-ordenada(L) :- 
-  //
+ordenada([]).
+ordenada([_]).
+ordenada([X, Y|L]) :-
+    X =< Y,
+    ordenada([Y|L]).
