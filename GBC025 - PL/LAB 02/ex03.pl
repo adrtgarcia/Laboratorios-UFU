3e95f0006bda2485c41b7f303f26c1e2ca33369e
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2023-11-12 12:34:22 -0300

    Create ex03.pl

---
+++ GBC025 - PL/LAB 02/ex03.pl
@@ -0,0 +1,47 @@
+% aluno(Nome, Disciplina, Nota).
+aluno(ana, calculo, 10).
+aluno(joão, logica, 8).
+aluno(maria, programacao, 9).
+aluno(pedro, programacao, 7).
+aluno(carolina, calculo, 9).
+aluno(rafael, calculo, 5).
+aluno(juliana, logica, 6).
+aluno(lucas, calculo, 8).
+aluno(beatriz, programacao, 10).
+aluno(thiago, calculo, 7).
+aluno(isabela, logica, 6).
+aluno(bruno, programacao, 5).
+aluno(larissa, calculo, 5).
+aluno(gabriel, logica, 6).
+aluno(renata, calculo, 10).
+aluno(marcelo, calculo, 10).
+aluno(camila, logica, 9).
+aluno(gustavo, programacao, 8).
+aluno(vanessa, programacao, 9).
+aluno(daniel,  logica, 10).
+
+% professor(Nome, Disciplina, Peso).
+professor(fernanda, logica, 2).
+professor(marcos, programacao, 3).
+professor(aline, calculo, 1).
+
+% nota(NomeAluno, Disciplina, Valor).
+nota(N, D, V) :-
+    aluno(N, D, NOTA),
+    professor(_, D, PESO),
+    V is NOTA * PESO / 5.
+
+% maiorNota(Disciplina, NomeAluno).
+maiorNota(Disciplina, NomeAluno) :-
+    aluno(NomeAluno, Disciplina, Nota),
+    \+ (aluno(_, Disciplina, OutraNota), OutraNota > Nota).
+
+% menorNota(Disciplina, NomeAluno).
+menorNota(Disciplina, NomeAluno) :-
+    aluno(NomeAluno, Disciplina, Nota),
+    \+ (aluno(_, Disciplina, OutraNota), OutraNota < Nota).
+
+% mairNotaGeral(NomeAluno).
+maiorNotaGeral(NomeAluno) :-
+    aluno(NomeAluno, _, Nota),
+    \+ (aluno(_, _, OutraNota), OutraNota > Nota).
