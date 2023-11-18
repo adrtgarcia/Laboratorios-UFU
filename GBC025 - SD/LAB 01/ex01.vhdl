9a71d374a8190d75fbba819b1b0e1a210b8d28f7
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2023-11-17 21:23:15 -0300

    Update ex01.vhdl

--- GBC025 - SD/LAB 01/ex01.vhdl
+++ GBC025 - SD/LAB 01/ex01.vhdl
@@ -1,10 +1,14 @@
-library ieee; use ieee.std.logic_1164.all;
+library ieee;
+use ieee.std.logic_1164.all;
+
 entity portaE is
-  port(A,B:in std.logic;
-  S:out std.logic);
+  port ( 
+    A, B : in std_logic;
+    S : out std_logic
+  );
 end entity portaE;
 
 architecture A_portaE of portaE is
 begin
-  S<=((A nand B) nand (A nand B);
+  S <= ((A nand B) nand (A nand B));
 end architecture A_portaE;
