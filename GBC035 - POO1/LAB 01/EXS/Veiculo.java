afd1b0b92397b0ea8d8f9d433b956b30dbcb0939
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2024-02-14 18:47:31 -0300

    Create Veiculo.java

---
+++ GBC035 - POO1/LAB 01/EXS/Veiculo.java
@@ -0,0 +1,46 @@
+public class Veiculo {
+	private String modelo;
+	private String marca;
+	private String tipo;
+	private boolean ligado;
+	private double cargaMax;
+	
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	public boolean isLigado() {
+		return ligado;
+	}
+	public void setLigado(boolean ligado) {
+		this.ligado = ligado;
+	}
+	
+	public double getCargaMax() {
+		return cargaMax;
+	}
+	public void setCargaMax(double cargaMax) {
+		this.cargaMax = cargaMax;
+	}
+	
+	public void partidaCarro() {
+		if (!isLigado()) setLigado(true);
+	}
+}
