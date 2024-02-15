0bd9a80425ed77d95bdba2f1e9fbcaa83835400d
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2024-02-15 11:43:21 -0300

    Create Onibus.java

---
+++ GBC035 - POO1/LAB 04/Onibus.java
@@ -0,0 +1,22 @@
+public class Onibus extends Veiculo{
+	private int assentos;
+	
+	public Onibus(String placa, int ano, int assentos) {
+		setPlaca(placa);
+		setAno(ano);
+		setAssentos(assentos);
+	}
+
+	public int getAssentos() {
+		return assentos;
+	}
+	public void setAssentos(int assentos) {
+		this.assentos = assentos;
+	}
+	
+	public void exibirDads() {
+		System.out.println("placa: " + getPlaca());
+		System.out.println("ano: " + getAno());
+		System.out.println("assentos: " + getAssentos());
+	}
+}
