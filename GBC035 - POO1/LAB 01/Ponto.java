424565a32aa5b17234b0908b4f5d3046d73895ba
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2024-02-21 23:04:59 -0300

    Update Ponto.java

--- GBC035 - POO1/LAB 01/Ponto.java
+++ GBC035 - POO1/LAB 01/Ponto.java
@@ -1,3 +1,5 @@
+package laboratorios;
+
 import static java.lang.Math.sqrt;
 import static java.lang.Math.pow;
 
@@ -8,6 +10,7 @@ public class Ponto {
 		public int getX() {
 			return x;
 		}
+		
 		public void setX(int valor) {
 			if (valor >= 0) x = valor;
 			else x = 0;
@@ -16,16 +19,22 @@ public class Ponto {
 		public int getY() {
 			return y;
 		}
+		
 		public void setY(int valor) {
 			if (valor >= 0) y = valor;
 			else y = 0;
 		}
 		
-		public double distancia (Ponto a, Ponto b) {
-			int xa = a.getX();
-			int xb = b.getX();
-			int ya = a.getY();
-			int yb = b.getY();
+		public void inicializarPonto (int x, int y) {
+			setX(x);
+			setY(y);
+		}
+		
+		public double distancia (Ponto p) {
+			int xa = getX();
+			int xb = p.getX();
+			int ya = getY();
+			int yb = p.getY();
 			
 			double d = sqrt((pow(xa - xb, 2)) + (pow(ya - yb, 2)));
 			return d;
