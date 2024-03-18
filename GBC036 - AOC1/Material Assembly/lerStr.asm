273f06204d9f6832ed995245a5cea905a12fe955
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2024-03-17 21:40:30 -0300

    Update lerStr.asm

--- GBC036 - AOC1/Material Assembly/lerStr.asm
+++ GBC036 - AOC1/Material Assembly/lerStr.asm
@@ -7,16 +7,16 @@ str:	.space	80			                  # declarar str como array de 80 bytes
 main:
 	li  $v0, 4
 	la  $a0, msg1
-	syscall				                        # imprime a string msg1
+	syscall				                  # imprime a string msg1
 	li  $v0, 8			
-	la  $a0, str			                    # $a0 <- &str[0] 
-	li  $a1, 80			                      # $a1 <- size
-	syscall				                        # ler string -> $v0
+	la  $a0, str			                  # $a0 <- &str[0] 
+	li  $a1, 80			                  # $a1 <- size
+	syscall				                  # ler string -> $v0
 	li  $v0, 4
 	la  $a0, msg2
-	syscall				                        # imprime a string msg2
+	syscall				                  # imprime a string msg2
 	li  $v0, 4
 	la  $a0, str
-	syscall				                        # imprime a string str
+	syscall				                  # imprime a string str
 	li  $v0, 10
-	syscall				                        # exit
+	syscall				                  # exit
