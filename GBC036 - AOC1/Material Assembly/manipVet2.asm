f415f439126be3e7372b0c10af6d1f953f4ba6ab
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2024-03-17 21:32:49 -0300

    Update manipVet2.asm

--- GBC036 - AOC1/Material Assembly/manipVet2.asm
+++ GBC036 - AOC1/Material Assembly/manipVet2.asm
@@ -1,26 +1,26 @@
 	.data						           # declarando dados
-array:		.word	1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-n:		    .word	10				   # tamanho do vetor
+array:		  .word	1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+n:		  .word	10				           # tamanho do vetor
 soma:		  .word	0				           # resultado da soma
 msg:		  .ascii	"resultado: "
 
 	.text
 	.globl main
 main:
-	xor $t0, $0, $t0					           # i <- 0
-	xor $t1, $t1, $t1					           # soma <- 0
+	xor  $t0, $0, $t0					   # i <- 0
+	xor  $t1, $t1, $t1					   # soma <- 0
 	la   $t2, n
 	lw   $t2, 0($t2)				           # $t2 <- n
 	la   $t3, array					           # $t3 <- &array[0]
 
 loop:   
-	slt  $t4, $t0, $t2				           # $t4 = (i < n) ? 1 : 0
-	beq  $t4, $zero, end				           # se (i < n) faça
-	lw  $t4, 0($t3)				           # $t4 <- v[i]
-	add  $t1, $t1, $t4				           # soma += v[i]
-	addiu   $t3, $t3, 4				           # apontador do vetor
+	slt    $t4, $t0, $t2				           # $t4 = (i < n) ? 1 : 0
+	beq    $t4, $zero, end				           # se (i < n) faça
+	lw     $t4, 0($t3)				           # $t4 <- v[i]
+	add    $t1, $t1, $t4				           # soma += v[i]
+	addiu  $t3, $t3, 4				           # apontador do vetor
 	addiu  $t, $t0, 1				           # i++
-	j    loop
+	j      loop
 
 end:
 	la   $t4, soma
