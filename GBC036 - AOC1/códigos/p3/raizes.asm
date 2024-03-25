c600748c38ab7e68bd95f2f97260c5f4b4144781
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2024-03-25 15:19:59 -0300

    Update raizes.asm

--- GBC036 - AOC1/códigos/p3/raizes.asm
+++ GBC036 - AOC1/códigos/p3/raizes.asm
@@ -1,4 +1,6 @@
-	.include "macros.asm"		# utilizando as funções read_float, print_str
+# P3 - raÃ­zes de segunda ordem
+
+	.include "macros.asm"		# utilizando as funÃ§Ãµes read_float, print_str
 
 	.data
 cte4: 	.float	4.0			# define a constante 4
@@ -6,13 +8,13 @@ cte4: 	.float	4.0			# define a constante 4
 	.text
 	.globl main
 main:
-	read_float($f0)			# lê float do teclado
+	read_float($f0)			# lÃª float do teclado
      	mov.s	$f1, $f0		# $f1 = a
      
-     	read_float($f0)			# lê float do teclado
+     	read_float($f0)			# lÃª float do teclado
      	mov.s  	$f2, $f0		# $f2 = b
      
-     	read_float($f0)			# lê float do teclado
+     	read_float($f0)			# lÃª float do teclado
      	mov.s  	$f3, $f0		# $f3 = c
      
      	mtc1   	$zero, $f5		# (fpu) $f5 = 0
@@ -29,27 +31,27 @@ main:
      	sub.s   $f0, $f0, $f4   	# $f0 = delta
      
      	c.lt.s 	$f0, $f5   		# if (delta < 0) flag = 0
-     	bc1t 	complexo     		# if (flag == 0) pula para a função complexo
+     	bc1t 	complexo     		# if (flag == 0) pula para a funÃ§Ã£o complexo
      
      	sqrt.s  $f6,$f0     		# $f6 = raiz quadrada de delta
      	sub.s   $f2, $f5, $f2   	# $f2 = -b
      
-     					# comparações para regra da tumba:     
+     					# comparaÃ§Ãµes para regra da tumba:     
      					# comparar b^2 > ac e b>0 para utilizar R1 com forma SEGURA e R2 com TRADICIONAL
      	mul.s   $f13, $f3, $f1		# $f13 = c * a
      	add.s   $f9, $f1, $f1     	# $f9 = 2a
      	add.s   $f14, $f3, $f3    	# $f14 = 2c
      
      	c.le.s  $f13, $f2   		# if (ac <= -b) flag = 0
-     	bc1t    primeiro_metodo		# if (flag == 0) pula para a função segundo método
-     	j       segundo_metodo		# else pula para a função primeiro método
+     	bc1t    primeiro_metodo		# if (flag == 0) pula para a funÃ§Ã£o segundo mÃ©todo
+     	j       segundo_metodo		# else pula para a funÃ§Ã£o primeiro mÃ©todo
      
 segundo_metodo:
      	add.s   $f7, $f2, $f6     	# $f7 = -b + raiz de delta
      	div.s   $f16, $f7, $f9    	# $f16 = $f7 / 2a (R1 TRADICIONAL)
      	div.s   $f15, $f14, $f7   	# $f15 = 2c / $f7 (R2 FORMA SEGURA)
      
-     	j 	imprimir_resultado	# jump para a impressão resultado
+     	j 	imprimir_resultado	# jump para a impressÃ£o resultado
     
 primeiro_metodo:
      	sub.s   $f7, $f2, $f6     	# $f7 = -b - raiz de delta
@@ -69,7 +71,7 @@ imprimir_resultado:
      	j 	fim			# jump para o fim do programa
 
 complexo:
-     	abs.s   $f0, $f0		# módulo de delta
+     	abs.s   $f0, $f0		# mÃ³dulo de delta
      	sqrt.s  $f6,$f0     		# sqrt(abs(b2-4ac))
      	sub.s   $f2, $f5, $f2		# b = 0 - b
      	add.s   $f9,$f1,$f1     	# 2*a
@@ -80,8 +82,8 @@ complexo:
      	mov.s   $f12, $f7   		# $f12 = parte real
      	print_float($f12)		# imprime parte real
      	print_str " +- "
-     	mov.s   $f12, $f8   		# $f12 = parte imaginária
-     	print_float($f12)		# imprime parte imaginária
+     	mov.s   $f12, $f8   		# $f12 = parte imaginÃ¡ria
+     	print_float($f12)		# imprime parte imaginÃ¡ria
      	print_str "i\n"
      	j fim
 
