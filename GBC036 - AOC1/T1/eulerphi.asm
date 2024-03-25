2b0fdeb745e31323b6ced174180a7817b14f6c6a
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2024-03-25 15:22:44 -0300

    Update and rename GBC036 - AOC1/códigos/p5/eulerphi.asm to GBC036 - AOC1/T1/eulerphi.asm

--- GBC036 - AOC1/T1/eulerphi.asm
+++ GBC036 - AOC1/T1/eulerphi.asm
@@ -1,22 +1,24 @@
+# P5 - fun√ß√£o euler phi
+
 	.data
 n: 	.word 	100  				# valor de entrada
-result: .word 	0  				# resultado da funÁ„o euler phi
-msg: 	.asciiz "O resultado È: "
+result: .word 	0  				# resultado da fun√ß√£o euler phi
+msg: 	.asciiz "O resultado √©: "
 
 	.text
 	.globl main
 main:
-	la 	$t0, n  			# carrega o endereÁo de n em $t0
+	la 	$t0, n  			# carrega o endere√ßo de n em $t0
     	lw 	$a0, 0($t0)  			# carrega o valor de n em $a0
     	addi 	$t1, $zero, 1  			# inicializa o contador
     	addi 	$t2, $zero, 0  			# inicializa o resultado
 
 loop:
     	beq 	$t1, $a0, fim  			# if ( n == contador) encerra o loop
-    	jal 	mdc  				# chama a funÁ„o mdc
+    	jal 	mdc  				# chama a fun√ß√£o mdc
     	beq 	$v0, 1, incrementar  		# if (mdc(n, i) = 1), incrementa o resultado
     	addi 	$t1, $t1, 1  			# contador++
-    	j 	loop				# volta para o comeÁo do loop
+    	j 	loop				# volta para o come√ßo do loop
 
 incrementar:
     	addi 	$t2, $t2, 1  			# resultado++
@@ -24,7 +26,7 @@ incrementar:
     	j 	loop				# volta para o loop anterior
 
 fim:
-    	la 	$t0, result  			# carrega o endereÁo de result em $t0
+    	la 	$t0, result  			# carrega o endere√ßo de result em $t0
     	sw 	$t2, 0($t0)  			# armazena o resultado em result
 
     	la 	$a0, msg			# imprime a string (syscall)
@@ -45,11 +47,11 @@ mdc:
 mdc_loop:
     	beq 	$t4, $zero, mdc_fim  		# if (i == 0) termina o loop
     	add 	$t5, $t3, $zero  		# copia n para $t5
-    	rem 	$t5, $t5, $t4  			# calcula resto da divis„o de n por i
+    	rem 	$t5, $t5, $t4  			# calcula resto da divis√£o de n por i
     	add 	$t3, $t4, $zero  		# copia i para n
     	add 	$t4, $t5, $zero  		# copia n mod i para i
-    	j 	mdc_loop			# volta para o comeÁo do loop
+    	j 	mdc_loop			# volta para o come√ßo do loop
 
 mdc_fim:
     	add 	$v0, $t3, $zero  		# $v0 = resultado
-    	jr 	$ra				# retorna o resultado para a funÁ„o loop
+    	jr 	$ra				# retorna o resultado para a fun√ß√£o loop
