002bfa7afd1b4056f16edf1378313a24c25cea4a
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2024-03-25 18:54:52 -0300

    Update expo2.asm

--- GBC036 - AOC1/T1/expo2.asm
+++ GBC036 - AOC1/T1/expo2.asm
@@ -9,28 +9,26 @@ result: .word 	1         			# resultado inicializada como 1 (fator nulo da multi
 	.globl expo2
 expo2:						# movendo cada variável para seus respectivos registradores
 	la	$t0, x				# $t0 recebe o endereço de x
-	lw	$a0, 0($t0)			# $a0 recebe o valor apontado por $t0
+	lw	$t0, 0($t0)			# $a0 recebe o valor apontado por $t0
 	la	$t1, n				# $t1 recebe o endereço de n
-	lw	$a1, 0($t1)			# $a1 recebe o valor apontado por $t1
+	lw	$t1, 0($t1)			# $a1 recebe o valor apontado por $t1
 	la	$t2, result			# $t2 recebe o endereço de result
 	lw	$t2, 0($t2)			# $t2 recebe o valor apontado por $t2
-	
-	j	teste				# jump para teste
 
 loop:
-    	beq 	$a1, $zero, fimloop   		# if (n == 0) saia do loop
-    	andi 	$t3, $a1, 1          		# if ($a1 == 1) $a1 tem valor ímpar, $t3 recebe 1
+    	beq 	$t1, $zero, fimloop   		# if (n == 0) saia do loop
+    	andi 	$t3, $t1, 1          		# if ($t1 == 1) $t1 tem valor ímpar, $t3 recebe 1
     	bnez 	$t3, impar           		# if ($t3 != 0) jump para ímpar
 
 par:						# n é par, eleva x ao quadrado
-   	mul 	$a0, $a0, $a0         		# x *= x
-    	srl 	$a1, $a1, 1           		# n /= 2 (deslocamento lógico para a direita)
+   	mul 	$t0, $t0, $t0         		# x *= x
+    	srl 	$t1, $t1, 1           		# n /= 2 (deslocamento lógico para a direita)
     	j 	loop				# jump para loop
 
 impar:						# n é ímpar, multiplica result por x e eleva x ao quadrado
-  	mul 	$a2, $a2, $a0         		# result *= x
-    	mul 	$a0, $a0, $a0         		# x *= x
-    	srl 	$a1, $a1, 1           		# n /= 2 (deslocamento lógico para a direita)
+  	mul 	$t2, $t2, $t0         		# result *= x
+    	mul 	$t0, $t0, $t0         		# x *= x
+    	srl 	$t1, $t1, 1           		# n /= 2 (deslocamento lógico para a direita)
     	j 	loop				# jump para loop
 
 
