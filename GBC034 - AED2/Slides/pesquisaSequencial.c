b9d70922a530f9f7557e85876e551e970707ec9b
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2024-04-12 08:16:38 -0300

    Create pesquisaSequencial.c

---
+++ GBC034 - AED2/Slides/pesquisaSequencial.c
@@ -0,0 +1,17 @@
+// pesquisa sequencial em vetor
+int buscaSequencial (int *vetor, int n, int chaveBusca) {
+  int i;
+  for (i = 0; i < n; i++) {
+    if (vetor[i] == chaveBusca) return 1;
+  }
+  return -1;
+}
+
+// pesquisa sequencial em lista
+tElem *pesquisaLista (tElem *prim, int chaveBusca) {
+  tElem *aux;
+  for (aux = prim; aux != NULL; aux = aux->prox) {
+    if (aux->campo == chaveBusca) return aux;
+  }
+  return NULL;
+}
