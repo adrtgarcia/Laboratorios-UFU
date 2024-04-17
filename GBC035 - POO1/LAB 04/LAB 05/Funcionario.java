afd5019b847fb673d205e0ecacd341f989e52675
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2024-04-17 14:48:13 -0300

    Create Funcionario.java

---
+++ GBC035 - POO1/LAB 04/LAB 05/Funcionario.java
@@ -0,0 +1,39 @@
+public abstract class Funcionario {
+  private String nome;
+  private String rg;
+  private double salarioBase;
+
+  public Funcionario(String nome, String rg, double salarioBase) {
+    setNome(nome);
+    setRg(rg);
+    setSalarioBase(salarioBase);
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public void setRg(String rg) {
+    this.rg = rg;
+  }
+
+  public void setSalarioBase(double salarioBase) {
+    this.salarioBase = salarioBase;
+  }
+
+  public String getNome() {
+    return this.nome;
+  }
+
+  public String getRg() {
+    return this.rg;
+  }
+
+  public double getSalarioBase() {
+    return this.salarioBase;
+  }
+
+  public double salarioTotal() {
+    return salarioBase;
+  }
+}
