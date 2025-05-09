f25ec9241259088c5551d8bfecaeb2b1e6c87bb9
Amanda Duarte <127355878+aduarte09@users.noreply.github.com>
2025-05-08 23:08:57 -0300

    Update Main.java

--- FAMAT31401 - PPP/LAB 08/Main.java
+++ FAMAT31401 - PPP/LAB 08/Main.java
@@ -2,22 +2,22 @@ public class Main {
    public static void main(String[] args) {
 
        // a) 0
-       ExpressaoAritimetica e1 = new Numero(0);
+       ExpressaoAritmetica e1 = new Numero(0);
        System.out.println("Expressão: " + e1.toString());
        System.out.println("Resultado: " + e1.getResultado());
 
        // b) 1 + 2
-       ExpressaoAritimetica e2 = new Operacao(new Numero(1), '+', new Numero(2));
+       ExpressaoAritmetica e2 = new Operacao(new Numero(1), '+', new Numero(2));
        System.out.println("Expressão: " + e2.toString());
        System.out.println("Resultado: " + e2.getResultado());
 
        // c) 1 * (2 + 3)
-       ExpressaoAritimetica e3 = new Operacao(new Numero(1), '*', new Operacao(new Numero(2), '+', new Numero(3)));
+       ExpressaoAritmetica e3 = new Operacao(new Numero(1), '*', new Operacao(new Numero(2), '+', new Numero(3)));
        System.out.println("Expressão: " + e3.toString());
        System.out.println("Resultado: " + e3.getResultado());
 
        // d) (2 * 3) + (4 / (5 - 3))
-       ExpressaoAritimetica e4 = new Operacao(new Operacao(new Numero(2), '*', new Numero(3)), '+', new Operacao(new Numero(4), '/', new Operacao(new Numero(5), '-', new Numero(3))));
+       ExpressaoAritmetica e4 = new Operacao(new Operacao(new Numero(2), '*', new Numero(3)), '+', new Operacao(new Numero(4), '/', new Operacao(new Numero(5), '-', new Numero(3))));
        System.out.println("Expressão: " + e4.toString());
        System.out.println("Resultado: " + e4.getResultado());
 
